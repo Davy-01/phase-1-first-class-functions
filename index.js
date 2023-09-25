7c62b7efa535348ce82159200b11b28e4c44b26a
Davis <daviesnyakiba@gmail.com>
2023-09-25 11:38:00 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function receivesAFunction(callback){
+    return callback()
+}
+
+
+function returnsANamedFunction(){
+    return function sum(a,b){
+        return a + b
+    }
+}
+
+
+function returnsAnAnonymousFunction(){
+    return function(){console.log ("Learning JS")}
+}
